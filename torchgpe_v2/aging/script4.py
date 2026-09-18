010172f63a60cdab73be6284d11a6e8c1a79abcc
Erenay Karacan <erenaykaracan@student-net-vpn-2826.intern.ethz.ch>
2026-09-18 18:02:37 +0300

    Automatic commit

--- torchgpe_v2/aging/script4.py
+++ torchgpe_v2/aging/script4.py
@@ -7,6 +7,14 @@ import torch
 import numpy as np
 
 
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+torch.set_num_threads(1)
+torch.set_num_interop_threads(1)
+
+
 # ============================================================
 # Arguments
 # ============================================================
